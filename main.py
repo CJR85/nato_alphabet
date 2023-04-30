1741a2df22e319f49ee3603d458c67f7e8298534
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-30 17:50:45 +0100

    Todo 1 solution

--- main.py
+++ main.py
@@ -20,8 +20,14 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(data.to_dict())
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
